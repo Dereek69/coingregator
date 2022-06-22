d7ee9c28b0124bdf9646b45c319c8bdf82689991
Tancredi Covioli <tancredi.covioli@gmail.com>
2022-06-23 00:55:24 +0200

    code formatting changes

--- updater/app.py
+++ updater/app.py
@@ -25,18 +25,26 @@ with open(COINS_FILE, "r") as file:
 
 
 COINGLASS_DICT = {
-    "funding_rates_u": ("https://open-api.coinglass.com/api/pro/v1/futures/"
-                        "funding_rates_chart?symbol={symbol}&type=U"),
-    "funding_rates_c": ("https://open-api.coinglass.com/api/pro/v1/futures/"
-                        "funding_rates_chart?symbol={symbol}&type=C"),
-    "open_interest": ("http://open-api.coinglass.com/api/pro/v1/futures/"
-                      "openInterest?interval=0&symbol={symbol}")
- }
+    "open_interest": (
+        "http://open-api.coinglass.com/api/pro/v1/futures/"
+        "openInterest?interval=0&symbol={symbol}"
+    ),
+    "funding_rates_u": (
+        "https://open-api.coinglass.com/api/pro/v1/futures/"
+        "funding_rates_chart?symbol={symbol}&type=U"
+    ),
+    "funding_rates_c": (
+        "https://open-api.coinglass.com/api/pro/v1/futures/"
+        "funding_rates_chart?symbol={symbol}&type=C"
+    ),
+}
 
-COINGECKO_URL = ("https://api.coingecko.com/api/v3/coins"
-                 "/markets?vs_currency=usd"
-                 "&order=market_cap_desc"
-                 "&per_page=250&page={page}&sparkline=false")
+COINGECKO_URL = (
+    "https://api.coingecko.com/api/v3/coins"
+    "/markets?vs_currency=usd"
+    "&order=market_cap_desc"
+    "&per_page=250&page={page}&sparkline=false"
+)
 
 
 if "REFRESH_RATE" in os.environ:
@@ -74,12 +82,17 @@ async def _coinglassRequests(rtype, symbol):
                 return (symbol, await response.text())
             else:
                 logging.getLogger(__name__).error(
-                    "request to {url} returned code {code}"
-                    .format(url=url, code=response.status))
-                raise Exception(("the request for symbol {symbol}"
-                                 " received a status"
-                                 " code {code}").format(symbol=symbol,
-                                                        code=response.status))
+                    "request to {url} returned code {code}".format(
+                        url=url, code=response.status
+                    )
+                )
+                raise Exception(
+                    (
+                        "the request for symbol {symbol}"
+                        " received a status"
+                        " code {code}"
+                    ).format(symbol=symbol, code=response.status)
+                )
 
 
 async def _coingeckoRequests(page):
@@ -88,42 +101,44 @@ async def _coingeckoRequests(page):
         async with session.get(url) as response:
             if response.status == 200:
                 logging.getLogger(__name__).info(
-                    "Coingecko request made for {url}".format(url=url))
+                    "Coingecko request made for {url}".format(url=url)
+                )
                 return (page, await response.text())
             else:
                 logging.getLogger(__name__).error(
-                    "request to {url} returned code {code}"
-                    .format(url=url, code=response.status))
-                raise Exception(("the request for page {page}"
-                                 " received a status"
-                                 " code {code}").format(page=page,
-                                                        code=response.status))
+                    "request to {url} returned code {code}".format(
+                        url=url, code=response.status
+                    )
+                )
+                raise Exception(
+                    (
+                        "the request for page {page}"
+                        " received a status"
+                        " code {code}"
+                    ).format(page=page, code=response.status)
+                )
 
 
 async def _coinglass_runner(rtype, redis):
-
     async def inner(requests):
         results = await asyncio.gather(*requests)
-        redis.mset({
-            f"{rtype}.{coin}": body
-            for (coin, body) in results
-        })
+        redis.mset({f"{rtype}.{coin}": body for (coin, body) in results})
 
-    for coin_set in [COINS[x:x+CONCURRENT_REQUESTS]
-                     if x < len(COINS)-CONCURRENT_REQUESTS
-                     else COINS[x:]
-                     for x in range(0, len(COINS), CONCURRENT_REQUESTS)]:
+    for coin_set in [
+        COINS[x : x + CONCURRENT_REQUESTS]
+        if x < len(COINS) - CONCURRENT_REQUESTS
+        else COINS[x:]
+        for x in range(0, len(COINS), CONCURRENT_REQUESTS)
+    ]:
         await inner([_coinglassRequests(rtype, coin) for coin in coin_set])
         await asyncio.sleep(INTER_REQUEST_TIME)
 
 
 async def _coingecko_runner(redis):
-    coingecko_results = await asyncio.gather(*[_coingeckoRequests(p)
-                                               for p in range(1, 7)])
-    redis.mset({
-        f"coingecko.{page}": body
-        for (page, body) in coingecko_results
-    })
+    coingecko_results = await asyncio.gather(
+        *[_coingeckoRequests(p) for p in range(1, 7)]
+    )
+    redis.mset({f"coingecko.{page}": body for (page, body) in coingecko_results})
 
 
 def main(r):
@@ -133,20 +148,20 @@ def main(r):
         asyncio.run(_coinglass_runner(rtype, r))
         logging.getLogger(__name__).info(
             "Finished the set of Coinglass requests"
-            "for type {rtype}".format(rtype=rtype))
+            "for type {rtype}".format(rtype=rtype)
+        )
+
 
 log_queue = queue.Queue()
 queue_handler = QueueHandler(log_queue)
 logging.basicConfig(level=logging.INFO)
 logging.getLogger().addHandler(queue_handler)
 stream_handler = StreamHandler(sys.stdout)
-queue_listener = QueueListener(log_queue,
-                               stream_handler)
+queue_listener = QueueListener(log_queue, stream_handler)
 queue_listener.start()
-r = redis.Redis(host='redis',
-                port=6379)
+r = redis.Redis(host="redis", port=6379)
 main(r)
 schedule.every(REFRESH_RATE).seconds.do(main, r)
-while(True):
+while True:
     schedule.run_pending()
     time.sleep(1)
