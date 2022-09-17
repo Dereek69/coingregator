4aec22ed9352d06218d398b66efccb5a605afbbc
Dereek69 <31802769+Dereek69@users.noreply.github.com>
2022-09-17 09:52:46 +0200

    Update app.py
    fixed coingecko solo prima pagina

--- updater/app.py
+++ updater/app.py
@@ -42,7 +42,7 @@ COINGLASS_DICT = {
 
 COINGECKO_URL = (
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
-    "&order=market_cap_desc&per_page=250&page=%7Bpage%7D"
+    "&order=market_cap_desc&per_page=250&page={page}"
     "&sparkline=false&price_change_percentage=24h%2C7d%2C30d"
 )
 
